15ec94e5a73692b2bd0eb41f9f82ba6c744d28dd
lac <laclys@126.com>
2017-06-17 13:27:15 +0800

    fetch post

--- FetchTest.js
+++ FetchTest.js
@@ -13,7 +13,6 @@ export default class FetchTest extends Component {
     fetch(url)
       .then(response => response.json())
       .then(result => {
-        console.log(result)
         this.setState({
           result: JSON.stringify(result)
         })
@@ -24,12 +23,37 @@ export default class FetchTest extends Component {
         })
       })
   }
+  onSubmit(url,data){
+    fetch(url,{
+      method:'post',
+      header:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(data)
+    })
+      .then(response=>response.json())
+      .then(result=>{
+        this.setState({
+          result:JSON.stringify(result)
+        })
+      })
+      .catch(error=>{
+        this.setState({
+          result:JSON.stringify(error)
+        })
+      })
+  }
   render() {
     return (
       <View style={styles.container}>
         <NavigationBar title={'Fetch的使用'}/>
-        <Text
-          onPress={() => this.onLoad('http://rap.taobao.org/mockjsdata/11793/test')}>获取数据</Text>
+        <Text style={styles.text}
+          onPress={() => this.onLoad('http://rap.taobao.org/mockjsdata/11793/test')}>获取数据
+        </Text>
+        <Text style={styles.text}
+          onPress={() => this.onSubmit('http://rap.taobao.org/mockjsdata/11793/submit',{"userName":"小明","password":"123456"})}>提交数据
+        </Text>
         <Text>返回结果：{this.state.result}</Text>
       </View>
     )
