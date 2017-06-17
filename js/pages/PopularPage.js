4aacea64cda6b302ee1a38dd63e964ed66538d7f
lac <laclys@126.com>
2017-06-18 00:55:51 +0800

    added ScrollableTabView Component and create PopularPage Component

--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 import NavigationBar from '../common/NavigationBar'
+import ScrollableTabView ,{ScrollableTabBar} from 'react-native-scrollable-tab-view';
 import HomePage from './HomePages'
 
 import DataRepository from '../expand/dao/DataRepository'
@@ -43,7 +44,15 @@ export default class PopularPage extends Component {
         title={'Popular'}
         style={{backgroundColor:'#6495ED'}}
       />
-      <Text 
+      <ScrollableTabView
+        renderTabBar={()=><ScrollableTabBar/>}
+      >
+        <PopularTab tabLabel="Java">Java</PopularTab>
+        <PopularTab tabLabel="IOS">IOS</PopularTab>
+        <PopularTab tabLabel="Vue">Vue</PopularTab>
+        <PopularTab tabLabel="Python">Python</PopularTab>
+      </ScrollableTabView>
+      {/*<Text 
         onPress={()=>{
           this.onLoad()
         }}
@@ -53,10 +62,51 @@ export default class PopularPage extends Component {
         style={{height:40,borderWidth:1}}
         onChangeText={text=>this.text=text}
       />
-      <Text style={{height:500}}>{this.state.result}</Text>
+      <Text style={{height:500}}>{this.state.result}</Text>*/}
     </View>
   }
 }
+
+class PopularTab extends Component{
+    constructor(props) {
+    super(props);
+    this.dataRepository=new DataRepository();
+    this.state={
+      result:''
+    }
+  }
+  componentDidMount(){
+    this.LoadData();
+  }
+  LoadData(){
+    let url=this.genUrl(this.props.tabLabel);
+    console.log(url);
+    this.dataRepository.fetchNetRepository(url)
+      .then(result=>{
+        console.log('成功')
+        this.setState({
+          result:JSON.stringify(result)
+        })
+      })
+      .catch(error=>{
+        console.log('失败')
+        this.setState({
+          result:JSON.stringify(error)
+        })
+      })
+  }
+  genUrl(key){
+    console.log('genUrl')
+    return URL + key + QUERY_STR;
+  }
+  render() {
+    return <View>
+            <Text style={{height:600}}>{this.state.result}</Text>      
+          </View> 
+  }
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1
