6716442ea976f6718130d27eb8e0efc1fe7215e3
lac <laclys@126.com>
2017-06-20 23:11:47 +0800

    import CheckBox Component

--- js/pages/my/CustomKeyPage.js
+++ js/pages/my/CustomKeyPage.js
@@ -3,6 +3,7 @@ import {View, Text, StyleSheet, Navigator,Image,TouchableOpacity,ScrollView} fro
 import NavigationBar from '../../common/NavigationBar'
 import ViewUtils from '../../util/ViewUtils'
 import LanguageDao,{FLAG_LANGUAGE} from '../../expand/dao/LanguageDao'
+import CheckBox from 'react-native-check-box'
 
 export default class CustomKeyPage extends Component {
   constructor(props) {
@@ -30,7 +31,49 @@ export default class CustomKeyPage extends Component {
     this.props.navigator.pop();
   }
   renderView(){
-    return <Text style={{height:400,width:400}}>{JSON.stringify(this.state.dataArray)}</Text>
+    if(!this.state.dataArray||this.state.dataArray.length===0)return null;
+    let len = this.state.dataArray.length;
+    let views=[];
+    for(let i=0,l=len-1;i<l;i+=2){
+      views.push(
+        <View key={i}>
+          <View style={styles.item}>
+              {this.renderCheckBox(this.state.dataArray[i])}
+              {this.renderCheckBox(this.state.dataArray[i+1])}
+          </View>
+          <View style={styles.line}></View>
+        </View>
+      )
+    }
+    views.push(
+      <View key={len-1}>
+        <View style={styles.item}>
+            {len%2 === 0 ? this.renderCheckBox(this.state.dataArray[len-2]):null}
+            {this.renderCheckBox(this.state.dataArray[len-1])}
+            <View style={styles.line}></View>
+        </View>
+      </View>
+    )
+    return views;
+  }
+  onClick(data){
+    
+  }
+  renderCheckBox(data){
+    leftText=data.name;
+    return (
+      <CheckBox
+        style={{flex:1}}
+        onClick={()=>this.onClick(data)}
+        leftText={leftText}  
+        checkedImage={<Image
+          source={require('./images/ic_check_box.png')}
+         />}
+        unCheckedImage={<Image
+          source={require('./images/ic_check_box_outline_blank.png')}
+         />}
+      />
+    )
   }
   render() {
     let rightButton=<TouchableOpacity
@@ -66,5 +109,13 @@ const styles = StyleSheet.create({
   btn:{
     fontSize:20,
     color:'white'
+  },
+  line:{
+    height:1,
+    backgroundColor:'black'
+  },
+  item:{
+    flexDirection:'row',
+    alignItems:'center'
   }
 });
