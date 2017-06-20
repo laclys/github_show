5d357ede838556f42e23ca42052779741a7786d9
lac <laclys@126.com>
2017-06-20 22:14:28 +0800

    取出key.json数据

--- js/pages/my/CustomKeyPage.js
+++ js/pages/my/CustomKeyPage.js
@@ -2,16 +2,35 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet, Navigator,Image,TouchableOpacity,ScrollView} from 'react-native';
 import NavigationBar from '../../common/NavigationBar'
 import ViewUtils from '../../util/ViewUtils'
+import LanguageDao,{FLAG_LANGUAGE} from '../../expand/dao/LanguageDao'
 
 export default class CustomKeyPage extends Component {
   constructor(props) {
     super(props);
+    this.languageDao=new LanguageDao(FLAG_LANGUAGE.flag_key);
+    this.state={
+      dataArray:[]
+    }
+  }
+  componentDidMount(){
+    this.loadData();
+  }
+  loadData(){
+    this.languageDao.fetch()
+        .then(result=>{
+          this.setState({
+            dataArray:result
+          })
+        })
+        .catch(error=>{
+          console.log(error);
+        })
   }
   onSave(){
     this.props.navigator.pop();
   }
   renderView(){
-    
+    return <Text style={{height:400,width:400}}>{JSON.stringify(this.state.dataArray)}</Text>
   }
   render() {
     let rightButton=<TouchableOpacity
