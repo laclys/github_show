f18e7b410318ef597db6fa841f111715157c75f8
lac <laclys@126.com>
2017-07-17 23:16:52 +0800

    项目详情页 添加收藏星星

--- js/pages/RepositoryDetail.js
+++ js/pages/RepositoryDetail.js
@@ -5,10 +5,13 @@ import {
   Text,
   WebView,
   TextInput,
-  DeviceEventEmitter
+  DeviceEventEmitter,
+  TouchableOpacity,
+  Image
 } from 'react-native'
-import NavigationBar from '../../js/common/NavigationBar'
-import ViewUtils from '../util/ViewUtils'
+import NavigationBar from '../../js/common/NavigationBar';
+import ViewUtils from '../util/ViewUtils';
+import FavoriteDao from '../expand/dao/FavoriteDao';
 
 const URL ="https://github.com/"
 export default class RepositoryDetail extends Component {
@@ -16,11 +19,16 @@ export default class RepositoryDetail extends Component {
     super(props);
     this.url=this.props.projectModel.item.html_url?this.props.projectModel.item.html_url
       :URL+this.props.projectModel.item.fullName;
-    let title=this.props.projectModel.item.full_name?this.props.projectModel.item.full_name:this.props.projectModel.item.fullName;
+    var title=this.props.projectModel.item.full_name?this.props.projectModel.item.full_name:this.props.projectModel.item.fullName;
+    this.favoriteDao = new FavoriteDao(this.flag)
     this.state = {
       url:this.url,
       title:title,
-      canGoBack:false
+      canGoBack:false,
+      isFavorite:this.props.projectModel.isFavorite,
+      favoriteIcon:this.props.projectModel.isFavorite?
+        require('../../res/images/ic_star.png')
+        :require('../../res/images/ic_unstar_navbar.png')      
     }
   }
   onBack(){
@@ -41,12 +49,45 @@ export default class RepositoryDetail extends Component {
       canGoBack:e.canGoBack,
     })
   }
+  onRightBtnClick() {
+    var projectModel = this.props.projectModel;
+    this.setFavoriteState(projectModel.isFavorite=!projectModel.isFavorite)
+    var key =projectModel.item.fullName?projectModel.item.fullName:projectModel.item.id.toString();
+    if(projectModel.isFavorite) {
+      this.favoriteDao.saveFavoriteItem(key,JSON.stringify(projectModel.item));
+    }else {
+      this.favoriteDao.removeFavoriteItem(key);
+    }
+  }
+  setFavoriteState(flag){
+    this.setState({
+      isFavorite:flag,
+      favoriteIcon:flag?require('../../res/images/ic_star.png')
+        :require('../../res/images/ic_unstar_navbar.png')    
+    })
+  }
+  renderRightBtn() {
+    console.log(this.state.favoriteIcon);
+    return <TouchableOpacity
+      onPress={()=>this.onRightBtnClick()}
+    >
+      <Image
+        style={{
+          width:20,
+          height:20,
+          marginRight:10
+        }}
+        source={this.state.favoriteIcon}
+      />
+    </TouchableOpacity>
+  }
   render() {
     return<View style={styles.container}>
       <NavigationBar
         title={this.state.title}
         style={{backgroundColor:'#6495ED'}}
         leftButton={ViewUtils.getLeftButton(()=>this.onBack())}
+        rightButton={this.renderRightBtn()}
       ></NavigationBar>
       <WebView
         ref={webView=>this.webView=webView}
