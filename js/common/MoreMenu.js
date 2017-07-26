5a8e9f8634d0b207f023c3da898bd0227efefa76
lac <laclys@126.com>
2017-07-27 00:02:50 +0800

    create 标签标示页

---
+++ js/common/MoreMenu.js
@@ -0,0 +1,13 @@
+/**
+ * 更多menu
+ */
+
+ export const MORE_MENU = {
+   Custom_Language:'Custom Language',
+   Sort_Language:'Sort Language',
+   Custom_Key:'Custom Key',
+   Sort_Key:'Sort Key',
+   Remove_Key:'Remove Key',
+   About_Author:'About Author',
+   About:'About',
+ }
