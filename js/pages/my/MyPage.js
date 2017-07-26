6c2006ef1a8ed5ed6684c7ebd7ebea89ea30eb0d
lac <laclys@126.com>
2017-07-27 00:08:21 +0800

    重构MyPage init

--- js/pages/my/MyPage.js
+++ js/pages/my/MyPage.js
@@ -1,92 +1,63 @@
 import React, {Component} from 'react'
-import {View, Text, StyleSheet, Navigator, Image} from 'react-native'
+import {View, Text, StyleSheet, Navigator, Image,ScrollView,TouchableHighlight} from 'react-native'
 import NavigationBar from '../../common/NavigationBar'
 import CustomKeyPage from './CustomKeyPage'
 import SortKeyPage from './SortKeyPage'
 import {FLAG_LANGUAGE} from '../../expand/dao/LanguageDao'
+import {MORE_MENU} from '../../common/MoreMenu'
+import GlobalStyles from '../../../res/styles/GlobalStyles'
 
 export default class MyPage extends Component {
   constructor (props) {
     super(props)
   }
+  onClick(tab) {
+
+  }
   render () {
-    return <View style={styles.container}>
-      <NavigationBar title='My' />
-      <Text
-        style={styles.text}
-        onPress={() => {
-          this
-          .props
-          .navigator
-          .push({
-            component: CustomKeyPage,
-            params: {
-              ...this.props,
-              flag: FLAG_LANGUAGE.flag_key
-            }
-          })
-        }}>自定义标签</Text>
-      <Text
-        style={styles.text}
-        onPress={() => {
-          this
-          .props
-          .navigator
-          .push({
-            component: CustomKeyPage,
-            params: {
-              ...this.props,
-              flag: FLAG_LANGUAGE.flag_language
-            }
-          })
-        }}>自定义语言</Text>
-      <Text
-        style={styles.text}
-        onPress={() => {
-          this
-          .props
-          .navigator
-          .push({
-            component: SortKeyPage,
-            params: {
-              ...this.props,
-              flag: FLAG_LANGUAGE.flag_key
-            }
-          })
-        }}>标签排序</Text>
-      <Text
-        style={styles.text}
-        onPress={() => {
-          this
-          .props
-          .navigator
-          .push({
-            component: SortKeyPage,
-            params: {
-              ...this.props,
-              flag: FLAG_LANGUAGE.flag_language
-            }
-          })
-        }}>语言排序</Text>
-      <Text
-        style={styles.text}
-        onPress={() => {
-          this
-          .props
-          .navigator
-          .push({
-            component: CustomKeyPage,
-            params: {...this.props, isRemoveKey: true}
-          })
-        }}>标签移除</Text>
-    </View>
+    var navigationBar = <NavigationBar 
+      title='My' 
+      style={{backgroundColor: '#6495ED'}}
+      />
+    return (
+    <View style={styles.container}>
+      {navigationBar}
+      <ScrollView>
+        <TouchableHighlight
+          onPress={()=>this.onClick(MORE_MENU.About)}
+        >
+          <View style={styles.item}>
+            <View style={{flexDirection:'row',alignItems:'center'}}>
+              <Image
+                style={[{width:40,height:40,marginRight:10,borderRadius:23}]}
+                source={require('./images/Bleach.png')}
+              />
+              <Text>GitHub Show~</Text>
+            </View>
+            <Image
+              style={[{
+                marginRight:10,
+                height:22,
+                width:22
+              },{tintColor:'#6495ED'}]}
+              source={require('../../../res/images/ic_tiaozhuan.png')}
+            />
+          </View>
+        </TouchableHighlight>
+        <View style={GlobalStyles.line} />
+      </ScrollView>
+    </View>)
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  text: {
-    fontSize: 29
+  item:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    padding:10,
+    height:60,
   }
 })
---
+++ res/styles/GlobalStyles.js
@@ -0,0 +1,12 @@
+/**
+ * 全局样式
+ */
+
+ module.exports = {
+   line:{
+     flex:1,
+     height:0.4,
+     opacity:0.5,
+     backgroundColor:'darkgray'
+   }
+ }
